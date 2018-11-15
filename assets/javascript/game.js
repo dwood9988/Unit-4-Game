cefea81a116396df0a1a2020931d433df1ddb41a
Dani Wood <daniwood@Danielles-MacBook-Pro.local>
2018-11-14 19:19:11 -0500

    added javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,73 @@
 // crystal game
 // start with four crystals and a randomly generated Goal number
 var goal;
-var wins;
-var losses;
-
-randomGoal = Math.floor(Math.random() * 69) + 30;
-console.log(randomGoal);
-
-$("#randomGoal").html("Goal: " + randomGoal);
-
-for (let i = 0; i < 4; i++) {
-    // each crystal gets a random number value from 1-12 
-    var randomValue = Math.floor(Math.random() * 11) + 1;
-    console.log(randomValue);
-
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": "crystal",
-        "data-random": randomValue
-
-    });
+var wins = 0;
+var losses = 0;
+var previousScore = 0;
 
 
+var beginGame = function () {
+    // empty the crystals at the start of each begin game function
+    $(".crystals").empty();
+    // assign a random goal for the user
+    var randomGoal = Math.floor(Math.random() * 101) + 19;
 
-    $(".crystals").append(crystal);
+
+    $("#randomGoal").html("Goal: " + "<br>" + randomGoal);
+
+// generates 4 crystals
+    for (let i = 0; i < 4; i++) {
+        // each crystal gets a random number value from 1-12 
+        var randomValue = Math.floor(Math.random() * 12) + 1;
+        console.log(randomValue);
+
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": "crystal",
+            "data-random": randomValue
+
+        });
+
+
+
+        $(".crystals").append(crystal);
+
+    }
 
 }
-$(".crystal").on('click', function () {
-    console.log($(this).attr("data-random"));
+
+beginGame();
+var reset = function () {
     
+}
+
+$(document).on('click', ".crystal", function () {
+
+    var crystalNumber = parseInt($(this).attr('data-random'));
+    previousScore += crystalNumber;
+    console.log(previousScore);
+
+    if (previousScore > randomGoal) {
+        console.log("YOU LOSE!");
+        losses++;
+        $("#losses").html("Losses:" + losses);
+        previousScore = 0;
+        console.log(losses);
+        beginGame();
+
+    }
+    else if (previousScore === randomGoal) {
+        console.log("YOU WIN!");
+        wins++;
+        $("#wins").html("Wins:" + wins);
+        previousScore = 0;
+        console.log(wins);
+        beginGame();
+
+    }
+
 });
- 
+
 
 // each crystal gets a new value every time you win or lose
 // when the user clicks a crystal, their total is increased by the number assigned to each crystal until it equals the total score.
